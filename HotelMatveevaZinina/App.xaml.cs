0b3ca95c95e290b1fc995b765feca6daba9de085
зинина.дарья <.@TERM1>
2024-11-05 10:28:37 +0300

    Добавьте файлы проекта.

---
+++ HotelMatveevaZinina/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using HotelMatveevaZinina.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelMatveevaZinina
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static ZininaMatveevaHotelEntities context = new ZininaMatveevaHotelEntities();
+        public static ZininaMatveevaHotelEntities GetContext()
+        {
+            if (context != null)
+            {
+                return context;
+            }
+            else
+            {
+                context = new ZininaMatveevaHotelEntities();
+                return context;
+            }
+        }
+    }
+}
